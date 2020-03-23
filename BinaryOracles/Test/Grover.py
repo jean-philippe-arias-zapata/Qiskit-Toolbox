21101eab9d6ef2ab9b1e84410c5318113cbc3b49
Jean Philippe Arias Zapata <jean-philippe.arias@quantfi.com>
2020-03-23 17:07:44 +0100

    Deletion of lines for test.

--- BinaryOracles/Test/Grover.py
+++ BinaryOracles/Test/Grover.py
@@ -1,4 +1,4 @@
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer, execute
+from qiskit import QuantumCircuit
 from qiskit.aqua.circuits.gates import mcry
 import numpy as np
 
@@ -68,25 +68,3 @@ def controlled_reflection_strictly_lower_than(circuit, ancillae_qubits, control_
     circuit.mcry(-np.pi/2, control_qubits, second_ancilla_qubit, ancillae_qubits[2:], mode='basic')
     circuit.mct(control_qubits, second_ancilla_qubit, ancillae_qubits[2:])
     return circuit
-
-
-backend = Aer.get_backend('qasm_simulator')
-shots = 10000
-
-for dist in range(4):
-    d = '0' + '0' * (3 - dist) + '1' + '0' * dist
-    for threshold in range(5):
-        ancillae_qubits = QuantumRegister(6)
-        control_qubits = QuantumRegister(1)
-        second_ancilla_qubit = ancillae_qubits[1]
-        threshold_qubits = QuantumRegister(4)
-        clbit = ClassicalRegister(1)
-        circuit = QuantumCircuit(ancillae_qubits, clbit)
-        circuit.add_register(threshold_qubits, control_qubits)
-        circuit.x(control_qubits)
-        circuit.x(threshold_qubits[dist])
-        circuit = controlled_strictly_lower_than_binary_oracle(circuit, ancillae_qubits, control_qubits, threshold_qubits, threshold)
-        circuit.measure(second_ancilla_qubit, clbit)
-        job_sim = execute(circuit, backend, shots=shots)
-        sim_result = job_sim.result()
-        print('threshold = ', threshold, ': ', d, ' => ', sim_result.get_counts(circuit))
