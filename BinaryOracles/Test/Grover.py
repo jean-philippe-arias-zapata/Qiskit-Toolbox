f6d7d660d4106d784596e4329f446f1fe7094ed3
Jean Philippe Arias Zapata <jean-philippe.arias@quantfi.com>
2020-03-23 17:03:04 +0100

    strictly_lower_than_binary_oracles methods compatible with any number of control qubits.

--- BinaryOracles/Test/Grover.py
+++ BinaryOracles/Test/Grover.py
@@ -42,7 +42,7 @@ def reflection_strictly_lower_than(circuit, ancillae_qubits, threshold_qubits, t
 ### CONTROLLED VERSIONS
     
 
-def controlled_strictly_lower_than_binary_oracle(circuit, ancillae_qubits, control_qubits, threshold_qubits, threshold): #FAIRE ATTENTION AU CAS AVEC UN SEUL QUBIT DE CONTROLE
+def controlled_strictly_lower_than_binary_oracle(circuit, ancillae_qubits, control_qubits, threshold_qubits, threshold):
     threshold = int(threshold)
     second_ancilla_qubit = ancillae_qubits[1]
     n_qubits = len(threshold_qubits)     
@@ -70,3 +70,23 @@ def controlled_reflection_strictly_lower_than(circuit, ancillae_qubits, control_
     return circuit
 
 
+backend = Aer.get_backend('qasm_simulator')
+shots = 10000
+
+for dist in range(4):
+    d = '0' + '0' * (3 - dist) + '1' + '0' * dist
+    for threshold in range(5):
+        ancillae_qubits = QuantumRegister(6)
+        control_qubits = QuantumRegister(1)
+        second_ancilla_qubit = ancillae_qubits[1]
+        threshold_qubits = QuantumRegister(4)
+        clbit = ClassicalRegister(1)
+        circuit = QuantumCircuit(ancillae_qubits, clbit)
+        circuit.add_register(threshold_qubits, control_qubits)
+        circuit.x(control_qubits)
+        circuit.x(threshold_qubits[dist])
+        circuit = controlled_strictly_lower_than_binary_oracle(circuit, ancillae_qubits, control_qubits, threshold_qubits, threshold)
+        circuit.measure(second_ancilla_qubit, clbit)
+        job_sim = execute(circuit, backend, shots=shots)
+        sim_result = job_sim.result()
+        print('threshold = ', threshold, ': ', d, ' => ', sim_result.get_counts(circuit))
