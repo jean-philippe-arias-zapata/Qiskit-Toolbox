2756e0082864b8467691182d0c8014c108722a3d
Jean Philippe Arias Zapata <jean-philippe.arias@quantfi.com>
2020-03-23 16:45:46 +0100

    Replacing the multi-controlled multi-target method QuantumCircuit.mcmt.
    The method QuantumCircuit.mcmt is not correct, it is better to use:
    - the multi-controlled toffoli method QuantumCircuit.mct;
    - the multi-controlled rotation-Y method mcry from qiskit-aqua.

--- BinaryOracles/Test/Grover.py
+++ BinaryOracles/Test/Grover.py
@@ -1,10 +1,12 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer, execute
+from qiskit.aqua.circuits.gates import mcry
+import numpy as np
 
 
 ### UNARY LOWER THAN
 
 
-def exact_lower_than_binary_oracle(circuit, ancillae_qubits, threshold_qubits, threshold):
+def strictly_lower_than_binary_oracle(circuit, ancillae_qubits, threshold_qubits, threshold):
     threshold = int(threshold)
     main_ancilla_qubit = ancillae_qubits[0]
     n_qubits = len(threshold_qubits)     
@@ -14,24 +16,24 @@ def exact_lower_than_binary_oracle(circuit, ancillae_qubits, threshold_qubits, t
         elif threshold <= 0:
                 raise NameError('This threshold cannot be studied because of lack of ancillae qubits.')
         else:
-            circuit.x(threshold_qubits[threshold:n_qubits])
-            circuit.mcmt(threshold_qubits[threshold:n_qubits], threshold_qubits[0:threshold], QuantumCircuit.cx, [main_ancilla_qubit])
-            circuit.x(threshold_qubits[threshold:n_qubits])
+            circuit.x(threshold_qubits[threshold:])
+            circuit.mct(threshold_qubits[threshold:], main_ancilla_qubit, threshold_qubits[0:threshold])
+            circuit.x(threshold_qubits[threshold:])
     else:
         if threshold > n_qubits - 1:
             circuit.x(main_ancilla_qubit)
         else:
-            circuit.x(threshold_qubits[max(0, threshold):n_qubits])
-            circuit.mcmt(threshold_qubits[max(0, threshold):n_qubits], ancillae_qubits[1:], QuantumCircuit.cx, [main_ancilla_qubit])
-            circuit.x(threshold_qubits[max(0, threshold):n_qubits])
+            circuit.x(threshold_qubits[max(0, threshold):])
+            circuit.mct(threshold_qubits[max(0, threshold):], main_ancilla_qubit, ancillae_qubits[1:])
+            circuit.x(threshold_qubits[max(0, threshold):])
     return circuit
 
 
-def reflection_exact_lower_than(circuit, ancillae_qubits, threshold_qubits, threshold):
+def reflection_strictly_lower_than(circuit, ancillae_qubits, threshold_qubits, threshold):
     main_ancilla_qubit = ancillae_qubits[0]
     circuit.x(main_ancilla_qubit)
     circuit.h(main_ancilla_qubit)
-    circuit = exact_lower_than_binary_oracle(circuit, ancillae_qubits, threshold_qubits, threshold)
+    circuit = strictly_lower_than_binary_oracle(circuit, ancillae_qubits, threshold_qubits, threshold)
     circuit.h(main_ancilla_qubit)
     circuit.x(main_ancilla_qubit)
     return circuit
@@ -40,7 +42,7 @@ def reflection_exact_lower_than(circuit, ancillae_qubits, threshold_qubits, thre
 ### CONTROLLED VERSIONS
     
 
-def controlled_exact_lower_than_binary_oracle(circuit, ancillae_qubits, control_qubits, threshold_qubits, threshold): ### FAIRE ATTENTION A LA SOMME D'UN ELEMENT AVEC UNE LISTE LIGNE 53
+def controlled_strictly_lower_than_binary_oracle(circuit, ancillae_qubits, control_qubits, threshold_qubits, threshold): #FAIRE ATTENTION AU CAS AVEC UN SEUL QUBIT DE CONTROLE
     threshold = int(threshold)
     second_ancilla_qubit = ancillae_qubits[1]
     n_qubits = len(threshold_qubits)     
@@ -48,43 +50,24 @@ def controlled_exact_lower_than_binary_oracle(circuit, ancillae_qubits, control_
         raise NameError('This threshold cannot be studied because of lack of ancillae qubits.')
     else:
         if threshold > n_qubits - 1:
-            circuit.mcmt(control_qubits, ancillae_qubits[2:], QuantumCircuit.cx, [second_ancilla_qubit])
+            circuit.mct(control_qubits, second_ancilla_qubit, ancillae_qubits[2:])
         else:
-            circuit.mcmt(control_qubits, ancillae_qubits[2:], QuantumCircuit.cx, threshold_qubits[max(0, threshold):n_qubits])
-            circuit.mcmt(list(control_qubits) + threshold_qubits[max(0, threshold):n_qubits], ancillae_qubits[2:], QuantumCircuit.cx, [second_ancilla_qubit])
-            circuit.mcmt(control_qubits, ancillae_qubits[2:], QuantumCircuit.cx, threshold_qubits[max(0, threshold):n_qubits])
+            for qubit in threshold_qubits[max(0, threshold):]:
+                circuit.mct(control_qubits, qubit, ancillae_qubits[2:])
+            circuit.mct(list(control_qubits) + threshold_qubits[max(0, threshold):], second_ancilla_qubit, ancillae_qubits[2:])
+            for qubit in threshold_qubits[max(0, threshold):]:
+                circuit.mct(control_qubits, qubit, ancillae_qubits[2:])
     return circuit
 
 
-def controlled_reflection_exact_lower_than(circuit, ancillae_qubits, threshold_qubits, control_qubits, threshold):
+def controlled_reflection_strictly_lower_than(circuit, ancillae_qubits, control_qubits, threshold_qubits, threshold):
     second_ancilla_qubit = ancillae_qubits[1]
-    circuit.mcmt(control_qubits, ancillae_qubits[2:], QuantumCircuit.cx, [second_ancilla_qubit])
+    circuit.mct(control_qubits, second_ancilla_qubit, ancillae_qubits[2:])
+    circuit.mcry(-np.pi/2, control_qubits, second_ancilla_qubit, ancillae_qubits[2:], mode='basic')
     circuit.mcmt(control_qubits, ancillae_qubits[2:], QuantumCircuit.ch, [second_ancilla_qubit])
-    circuit = controlled_exact_lower_than_binary_oracle(circuit, ancillae_qubits, threshold_qubits, control_qubits, threshold)
-    circuit.mcmt(control_qubits, ancillae_qubits[2:], QuantumCircuit.ch, [second_ancilla_qubit])
-    circuit.mcmt(control_qubits, ancillae_qubits[2:], QuantumCircuit.cx, [second_ancilla_qubit])
+    circuit = controlled_strictly_lower_than_binary_oracle(circuit, ancillae_qubits, control_qubits, threshold_qubits, threshold)
+    circuit.mcry(-np.pi/2, control_qubits, second_ancilla_qubit, ancillae_qubits[2:], mode='basic')
+    circuit.mct(control_qubits, second_ancilla_qubit, ancillae_qubits[2:])
     return circuit
-
-backend = Aer.get_backend('qasm_simulator')
-shots = 10000
-
-for dist in range(4):
-    d = '0' + '0' * (3 - dist) + '1' + '0' * dist
-    for threshold in range(5):
-        ancillae_qubits = QuantumRegister(4)
-        main_ancilla_qubit = ancillae_qubits[0]
-        threshold_qubits = QuantumRegister(4)
-        clbit = ClassicalRegister(1)
-        circuit = QuantumCircuit(ancillae_qubits, clbit)
-        circuit.add_register(threshold_qubits)
-        circuit.x(threshold_qubits[dist])
-        circuit = exact_lower_than_binary_oracle(circuit, ancillae_qubits, threshold_qubits, threshold)
-        circuit.measure(main_ancilla_qubit, clbit)
-        job_sim = execute(circuit, backend, shots=shots)
-        sim_result = job_sim.result()
-        print('threshold = ', threshold, ': ', d, ' => ', sim_result.get_counts(circuit))
-
-
-
 
 
