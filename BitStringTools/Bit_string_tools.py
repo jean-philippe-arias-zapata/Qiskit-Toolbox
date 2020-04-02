01269740b354bc8612600464a7c54eda993ab4c3
Jean Philippe Arias Zapata <jean-philippe.arias@quantfi.com>
2020-04-02 19:08:42 +0200

    Adding of the XRegionGate.

--- BitStringTools/Bit_string_tools.py
+++ BitStringTools/Bit_string_tools.py
@@ -1,3 +1,7 @@
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.circuit import Gate
+from qiskit.extensions.standard.x import XGate
+
 
 def to_bin(region, step):
     to_bin = format(region, 'b')
@@ -23,3 +27,19 @@ def x_gates_region(circuit, qubits, string):
         if string[i] == '0':
               circuit.x(qubits[n_qubits - i - 1])
     return circuit 
+
+
+class XRegionGate(Gate):
+    """X-Region gate. """
+    
+    def __init__(self, num_qubits, string):
+        self.num_qubits = num_qubits
+        self.string = string
+        super().__init__(name=f"XRegion({string})", num_qubits=num_qubits, params=[string])
+    
+    def _define(self):
+        self.definition = []
+        q = QuantumRegister(self.num_qubits)
+        for i in range(len(self.string)):
+            if self.string[i] == '0':
+                self.definition.append((XGate(), [q[self.num_qubits - i - 1]], []))
