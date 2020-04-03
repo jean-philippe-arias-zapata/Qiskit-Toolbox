7b19cd340ba2e2d6c79394613619678144ae7a82
Jean Philippe Arias Zapata <jean-philippe.arias@quantfi.com>
2020-04-03 17:29:17 +0200

    Modification of the import for the method is_stochastic_vector.

--- Encoding/Unary-Encoding/UnaryEncoding.py
+++ Encoding/Unary-Encoding/UnaryEncoding.py
@@ -4,7 +4,9 @@ from qiskit.extensions.standard.x import CnotGate, XGate
 from qiskit.extensions.standard.ry import CryGate
 import os
 os.chdir('../../Preprocessing')
-from data_preparation import is_stochastic_vector
+from Classical_boolean_tests import is_stochastic_vector
+os.chdir('../Visualization')
+from Gate_visualization_tools import simp_pi_fy
 os.chdir('../Encoding/Unary-Encoding')
 import numpy as np
 
@@ -34,7 +36,7 @@ class PartialSwapGate(Gate):
     
     def __init__(self, angle):
         self.angle = angle
-        super().__init__(name=f"PartialSwap({angle})", num_qubits=2, params=[angle])
+        super().__init__(name=f"PartialSwap(" + simp_pi_fy(angle, 10**(-3), 10) + ")", num_qubits=2, params=[angle])
     
     def _define(self):
         definition = []
