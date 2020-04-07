04781cc0f157b14fa0f9f2b9e16ca9b00742de14
Jean Philippe Arias Zapata <jean-philippe.arias@quantfi.com>
2020-04-07 22:27:59 +0200

    Modification and displacement of the Quantum adder gate file.

--- QuantumAddition/QuantumAdderGate.py
+++ QuantumAddition/QuantumAdderGate.py
@@ -3,7 +3,7 @@ from qiskit.extensions.standard.u1 import Cu1Gate
 from qiskit import QuantumRegister, QuantumCircuit, ClassicalRegister, execute, Aer
 import os
 os.chdir('../QFT')
-from QFTGate import QFTGate
+from QFTGate import QFTGate, DoSwapsGate
 os.chdir('../QuantumAddition')
 from math import pi
 
@@ -22,19 +22,24 @@ class QuantumAdderGate(Gate):
             n = self.num_qubits//2
             x = q[:n]
             y = q[n:]
-            self.definition.append((QFTGate(n), y, []))
+            self.definition.append((DoSwapsGate(n), y, []))
+            self.definition.append((QFTGate(n, False), y, []))
             for i in range(n):
                 for j in range(n - i):
                     self.definition.append((Cu1Gate(pi / 2 ** j), [x[i], y[n - 1 - i - j]], []))
-            self.definition.append((QFTGate(n).inverse(), y, []))
+            self.definition.append((QFTGate(n, False).inverse(), y, []))
+            self.definition.append((DoSwapsGate(n), y, []))
 
             
 qr = QuantumRegister(4)
+x = qr[:2]
+y = qr[2:]
 cl = ClassicalRegister(2)
 circ = QuantumCircuit(qr, cl)
-circ.x(qr[0])
+circ.x(x)
+circ.x(y)
 circ.append(QuantumAdderGate(4), qr, [])
-circ.measure(qr[2:], cl)
+circ.measure(y, cl)
 
 backend = Aer.get_backend('qasm_simulator')
 
