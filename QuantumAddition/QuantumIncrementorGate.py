b656374ec8e350e5fe2013f4cb11c21bfc6ea972
Jean Philippe Arias Zapata <jean-philippe.arias@quantfi.com>
2020-04-08 12:37:59 +0200

    Modifications to be in the Qiskit convention.

--- QuantumAddition/QuantumIncrementorGate.py
+++ QuantumAddition/QuantumIncrementorGate.py
@@ -2,11 +2,15 @@ from math import pi
 import os
 os.chdir('../QFT')
 from QFTGate import QFTGate
-from QFT import quantum_fourier_transform
 os.chdir('../QuantumAddition')
 from qiskit.circuit import Gate
-from qiskit import QuantumRegister, QuantumCircuit
+from qiskit import QuantumRegister
 from qiskit.extensions.standard.u1 import U1Gate
+
+
+# We are in the Qiskit convention, i. e. the least significant bit first convention.
+# To go to the most significant bit first convention, you need to :
+#   - Add a SWAP gate acting on q at the end of the algorithm.  
 
 
 class QuantumIncrementorGate(Gate):
@@ -19,7 +23,7 @@ class QuantumIncrementorGate(Gate):
     def _define(self):
         self.definition = []
         q = QuantumRegister(self.num_qubits)
-        self.definition.append((QFTGate(self.num_qubits), q, []))
+        self.definition.append((QFTGate(self.num_qubits, bool_swaps=False), q, []))
         for i in range(self.num_qubits):
             self.definition.append((U1Gate(float(pi * self.params[0])/2**i), [q[self.num_qubits - i - 1]], []))
-        self.definition.append((QFTGate(self.num_qubits).inverse(), q, []))
+        self.definition.append((QFTGate(self.num_qubits, bool_swaps=False).inverse(), q, []))
--- QuantumAddition/Quantum_incrementor.py
+++ QuantumAddition/Quantum_incrementor.py
@@ -5,20 +5,25 @@ from QFT import quantum_fourier_transform, controlled_quantum_fourier_transform
 os.chdir('../QuantumAddition')
 
 
+# We are in the Qiskit convention, i. e. the least significant bit first convention.
+# To go to the most significant bit first convention, you need to :
+#   - Add a SWAP gate acting on the target_qubits at the end of the algorithm.
+
+
 def quantum_incrementor(epsilon, circuit):
     qubits = circuit.qubits
     n_qubits = circuit.n_qubits
-    circuit = quantum_fourier_transform(circuit, qubits)
+    circuit = quantum_fourier_transform(circuit, qubits, inverse=False, bool_swaps=False)
     for i in range(n_qubits):
         circuit.u1(float(pi * epsilon)/2**i , qubits[n_qubits - i - 1])
-    circuit = quantum_fourier_transform(circuit, qubits, inverse=True)
+    circuit = quantum_fourier_transform(circuit, qubits, inverse=True, bool_swaps=False)
     return circuit
 
 
 def controlled_quantum_incrementor(epsilon, circuit, ctrl_qubits, ancillae_qubits, target_qubits):
     n_target = len(target_qubits)
-    circuit = controlled_quantum_fourier_transform(circuit, ctrl_qubits, ancillae_qubits, target_qubits)
+    circuit = controlled_quantum_fourier_transform(circuit, ctrl_qubits, ancillae_qubits, target_qubits, inverse=False, bool_swaps=False)
     for i in range(n_target):
         circuit.mcu1(float(pi * epsilon)/2**i, ctrl_qubits, target_qubits[n_target - i - 1])
-    circuit = controlled_quantum_fourier_transform(circuit, ctrl_qubits, ancillae_qubits, target_qubits, inverse=True)
+    circuit = controlled_quantum_fourier_transform(circuit, ctrl_qubits, ancillae_qubits, target_qubits, inverse=True, bool_swaps=False)
     return circuit
